ff39b46d6f9814fb2bad4910d8e8dd1cfb71dd9d
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-14 23:44:50 +0500

    Exercise3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = 4;
+int b = -3;
+int c = 7;
+if (a % 2 == 0)
+{
+    Console.Write (a);
+    Console.WriteLine (" - четное");
+}
+else
+{
+    Console.Write (a);
+    Console.WriteLine (" - нечетное");
+}
+if (b % 2 == 0)
+{
+    Console.Write (b);
+    Console.WriteLine (" - четное");
+}
+else
+{
+    Console.Write (b);
+    Console.WriteLine (" - нечетное");
+}
+if (c % 2 == 0)
+{
+    Console.Write (c);
+    Console.WriteLine (" - четное");
+}
+else
+{
+    Console.Write (c);
+    Console.WriteLine (" - нечетное");
+}
