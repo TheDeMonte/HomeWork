3cc5612feb32e7a44084301e416ba9225de1c709
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-14 23:22:10 +0500

    Exercise 2

---
+++ Exercise2.1/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 44;
+int b = 5;
+int c = 78;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write ("Максимальное число: ");
+Console.Write (max);
---
+++ Exercise2.2/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 22;
+int b = 3;
+int c = 9;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write ("Максимальное число: ");
+Console.Write (max);
