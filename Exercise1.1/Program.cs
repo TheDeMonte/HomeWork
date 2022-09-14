d71ca4c0b9ece8f04f903eb2ed504018d577c949
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-14 23:07:01 +0500

    Exercise 1.1

---
+++ Exercise1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 2;
+int b = 10;
+int max = a;
+int min = b;
+
+if (b > max)
+{
+    max = b; min = a;
+    Console.Write ("Максимальное число: ");
+    Console.WriteLine (max);
+    Console.Write ("Минимальное число: ");
+    Console.Write (min);
+}
+else 
+{
+    max = a; min = b;
+    Console.Write ("Максимальное число: ");
+    Console.WriteLine (max);
+    Console.Write ("Минимальное число: ");
+    Console.Write (min);
+}
