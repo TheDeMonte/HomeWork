a32fa45b2fe4500e7e686a9b4c0d5f8b5301cf6d
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-15 00:05:45 +0500

    Exercise4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,14 @@
+﻿int x = 1;
+while (x < 5)
+{
+    if (x % 2 == 0)
+    Console.WriteLine(x);
+    x++;
+}   
+int a = 1;             
+while (a <= 8)
+{
+    if (a % 2 == 0)
+    Console.WriteLine(a);
+    a++;
+}
