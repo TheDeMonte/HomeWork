073e515d8432004b24e2e52f0e3f8642fd374fda
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-16 10:43:17 +0500

    Some fixes

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,33 +1,35 @@
-﻿int a = 4;
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int a = 4;
 int b = -3;
 int c = 7;
 if (a % 2 == 0)
 {
     Console.Write (a);
-    Console.WriteLine (" - четное");
+    Console.WriteLine (" - Является четным числом");
 }
 else
 {
     Console.Write (a);
-    Console.WriteLine (" - нечетное");
+    Console.WriteLine (" - Не является четным числом");
 }
 if (b % 2 == 0)
 {
     Console.Write (b);
-    Console.WriteLine (" - четное");
+    Console.WriteLine (" - является четным числом");
 }
 else
 {
     Console.Write (b);
-    Console.WriteLine (" - нечетное");
+    Console.WriteLine (" - не является четным числом");
 }
 if (c % 2 == 0)
 {
     Console.Write (c);
-    Console.WriteLine (" - четное");
+    Console.WriteLine (" - является четным числом");
 }
 else
 {
     Console.Write (c);
-    Console.WriteLine (" - нечетное");
+    Console.WriteLine (" - не является четным числом");
 }
